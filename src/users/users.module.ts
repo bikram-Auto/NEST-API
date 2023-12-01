0197171e12d3933c17e9975c76b72767eecea99e
Bikram <bikram.nanda@squbix.com>
2023-12-01 12:31:51 +0530

    document user controller and module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { MongoDBService } from 'src/function/mongodb.service';
 
-
+/**
+ * NestJS module for managing user-related functionality.
+ * This module encapsulates the users controller and MongoDB service.
+ */
 @Module({
+  // Controllers that handle HTTP requests related to users
   controllers: [UsersController],
+
+  // Providers (services, repositories, etc.) used within the module
   providers: [MongoDBService],
 })
-export class UsersModule {
-
-}
-
+export class UsersModule {}
