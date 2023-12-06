c419a3ff72faec22fed7aec5ea70e9bbb54b3782
Bikram <bikram.nanda@squbix.com>
2023-12-06 11:24:46 +0530

    add swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 /**
  * Bootstrap the NestJS application.
@@ -8,6 +9,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // Create a Nest application instance based on the AppModule
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('create user')
+    .setDescription('Create User and get and update user by data, name and date')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Start the application by listening on a specified port (e.g., 3000)
   await app.listen(3000);
